5d0f9f1d8ecbe86602ade7d207e297c141be6dca
Shannon Dunn <dunn.shannon@ge.com>
2015-05-31 22:06:32 -0700

    first cut at minor testa

--- spec/christ_spec.rb
+++ spec/christ_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
-
 describe Christ do
+  before :each do
+    @gist = Christ::Gist.new
+  end
   it 'has a version number' do
     expect(Christ::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#github_connect" do
+      it "returns an octokit object" do
+          @gist.github_connect.should be_an_instance_of Octokit::Client
+      end
   end
 end
